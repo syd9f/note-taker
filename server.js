8577074c8011020a7cce694014256d84257135ad
sydnief <sydniefarrell@gmail.com>
2023-04-16 18:48:48 -0400

    used app.get to navigate between pages

--- server.js
+++ server.js
@@ -3,3 +3,18 @@ const path = require('path');
 
 const app = express();
 const PORT = 3001;
+
+app.use(express.static('public'));
+app.get('/', (req, res) => res.send('Navigate to /index or /notes'));
+
+app.get('/index', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`app listening at http://localhost:${PORT}`)
+);
