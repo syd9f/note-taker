64f1728e707fc1275d8cd0d2a1d3d805c3134686
sydnief <sydniefarrell@gmail.com>
2023-04-16 23:20:40 -0400

    can now save a nowto db.json file using user input

--- public/index.js
+++ public/index.js
@@ -1,36 +1,78 @@
 // select page elements
 const savedNotes = document.getElementById('saved-notes');
 const saveButton = document.getElementById('save-btn');
+const newNoteTitle = document.getElementById('note-name');
+const newNoteText = document.getElementById('note-text');
+
+// Helper function that accepts a `note` object, sends a POST request and returns the result
+const saveNote = (note) =>
+  // Fetch accepts a URL and an options object where you can declare the HTTP method, the request body, and any headers.
+  fetch('/api/db', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(note),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('Successful POST request:', data);
+      return data;
+    })
+    .catch((error) => {
+      console.error('Error in POST request:', error);
+    });
+
+// Listen for when the note is saved
+saveButton.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  // Create a new note object from the input values
+  const newNote = {
+    title: newNoteTitle.value.trim(),
+    body: newNoteText.value.trim(),
+  };
+
+  // Call our postReview method to make a POST request with our `newReview` object.
+  saveNote(newNote)
+    .then((data) => alert(`Note saved! Note Title: ${data.body.title}`))
+    .catch((err) => console.error(err));
+});
+
+
+// post a new note
+  
+  // update a note
 
 // get saved notes from db
-const getNotes = async () => {
-  const result = await fetch('/api/db', {
-    method: 'GET',
-  });
-  const json = await result.json();
-  return json;
-};
+// const getNotes = async () => {
+//   const result = await fetch('/api/db', {
+//     method: 'GET',
+//   });
+//   const json = await result.json();
+//   return json;
+// };
 
 // render notes to page
-const renderNotes = (note) => {
-    const cardEl = document.createElement('div');
-    const cardBodyEl = document.createElement('div');
-    const cardBodyTitle = document.createElement('div');
+// const renderNotes = (note) => {
+    // const cardEl = document.createElement('div');
+    // const cardBodyEl = document.createElement('div');
+    // const cardBodyTitle = document.createElement('div');
   
-    cardEl.classList.add('card', 'p-5');
-    cardBodyEl.classList.add('card-body', 'p-5');
-    cardBodyTitle.classList.add('card-header', 'card-title', 'link');
+    // cardEl.classList.add('card', 'p-5');
+    // cardBodyEl.classList.add('card-body', 'p-5');
+    // cardBodyTitle.classList.add('card-header', 'card-title', 'link');
   
-    cardBodyTitle.innerHTML = `<a href=${note.url}>${note.note}</a>`;
-    cardBodyEl.innerText = note.definition;
-    savedNotes.appendChild(cardBodyTitle);
-    savedNotes.appendChild(cardBodyEl);
-};
+    // cardBodyTitle.innerHTML = `<a href=${note.url}>${note.note}</a>`;
+    // cardBodyEl.innerText = note.definition;
+    // savedNotes.appendChild(cardBodyTitle);
+    // savedNotes.appendChild(cardBodyEl);
+// };
   
 // starts functions when button is clicked
-const buttonHandler = () =>
-    getNotes().then((response) => response.forEach((note) => renderNotes(note)));
+// const buttonHandler = () =>
+//     getNotes().then((response) => response.forEach((note) => renderNotes(note)));
 
-// event listener on save button
-saveButton.addEventListener('click', buttonHandler);
+// // event listener on save button
+// saveButton.addEventListener('click', buttonHandler);
 
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3001;
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static('public'));
 app.get('/', (req, res) => res.send('Navigate to /index or /notes'));
@@ -18,7 +19,47 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/pages/notes.html'))
 );
   
-// app.get('/api/db', (req, res) => res.json(notesData));
+app.get('/api/db', (req, res) => {
+    res.status(200).json(body);
+    res.json(`${req.method} request received to get saved notes`);
+    console.info(`${req.method} request received to get saved notes`);
+});
+
+// POST request to add a note
+app.post('/api/db', (req, res) => {
+    console.info(`${req.method} request received to save a note`);
+
+    const { title, body } = req.body;
+    
+    if (title && body) {
+      const newNote = {
+        title,
+        body,
+      };
+
+      const noteString = JSON.stringify(newNote);
+
+      fs.writeFile(`./db/db.json`, noteString, (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+            `New note: ${newNote.title} has been written to JSON file`
+          )
+    );
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting note');
+    }
+});
+
+
 
 app.listen(PORT, () =>
   console.log(`app listening at http://localhost:${PORT}`)
