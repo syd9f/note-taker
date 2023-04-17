4bd9cc86b65b7f00a5baa1e1feae9da678a582e6
sydnief <sydniefarrell@gmail.com>
2023-04-16 20:59:51 -0400

    started added functions for displaying saved notes to the page

--- public/index.js
+++ public/index.js
@@ -1,1 +1,36 @@
-console.log('index.js is working')
+// select page elements
+const savedNotes = document.getElementById('saved-notes');
+const saveButton = document.getElementById('save-btn');
+
+// get saved notes from db
+const getNotes = async () => {
+  const result = await fetch('/api/db', {
+    method: 'GET',
+  });
+  const json = await result.json();
+  return json;
+};
+
+// render notes to page
+const renderNotes = (note) => {
+    const cardEl = document.createElement('div');
+    const cardBodyEl = document.createElement('div');
+    const cardBodyTitle = document.createElement('div');
+  
+    cardEl.classList.add('card', 'p-5');
+    cardBodyEl.classList.add('card-body', 'p-5');
+    cardBodyTitle.classList.add('card-header', 'card-title', 'link');
+  
+    cardBodyTitle.innerHTML = `<a href=${note.url}>${note.note}</a>`;
+    cardBodyEl.innerText = note.definition;
+    savedNotes.appendChild(cardBodyTitle);
+    savedNotes.appendChild(cardBodyEl);
+};
+  
+// starts functions when button is clicked
+const buttonHandler = () =>
+    getNotes().then((response) => response.forEach((note) => renderNotes(note)));
+
+// event listener on save button
+saveButton.addEventListener('click', buttonHandler);
+
