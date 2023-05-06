d15106302daa84194b21c1ab68e4c37d97badb3f
sydnief <sydniefarrell@gmail.com>
2023-05-05 20:10:48 -0400

    deploy for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const util = require('util');
 const {v4: uuidv4} = require('uuid');
 
 const app = express();
-const PORT = 3001;
+const PORT = (process.env.PORT || 3001);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true }));
